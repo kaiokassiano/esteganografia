dc0632a50a53ecc960a121b1696768c43979417e
Kaio <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-03-20 23:21:58 -0300

    Update Vigenere.java

--- bin/Vigenere.java
+++ bin/Vigenere.java
@@ -7,30 +7,38 @@ import java.util.Scanner;
 
 public class Vigenere {
 
-	Scanner scanner = new Scanner(System.in);
+	private Scanner scanner = new Scanner(System.in);
 
-	public static String encrypt(String text, String key) {
+	// A chave tem valor padrao
+	private static String chave = "DISCRETA";
+
+	public static String encrypt(String text) throws Exception {
+
+		if (text.trim().isEmpty()) {
+			throw new Exception("Mensagem nao pode ser vazia!");
+		}
+
 		String res = "";
 		text = text.toUpperCase();
 		for (int i = 0, j = 0; i < text.length(); i++) {
 			char c = text.charAt(i);
 			if (c < 'A' || c > 'Z')
 				continue;
-			res += (char) ((c + key.charAt(j) - 2 * 'A') % 26 + 'A');
-			j = ++j % key.length();
+			res += (char) ((c + chave.charAt(j) - 2 * 'A') % 26 + 'A');
+			j = ++j % chave.length();
 		}
 		return res;
 	}
 
-	public static String decrypt(String text, String key) {
+	public static String decrypt(String text) {
 		String res = "";
 		text = text.toUpperCase();
 		for (int i = 0, j = 0; i < text.length(); i++) {
 			char c = text.charAt(i);
 			if (c < 'A' || c > 'Z')
 				continue;
-			res += (char) ((c - key.charAt(j) + 26) % 26 + 'A');
-			j = ++j % key.length();
+			res += (char) ((c - chave.charAt(j) + 26) % 26 + 'A');
+			j = ++j % chave.length();
 		}
 		return res;
 	}
