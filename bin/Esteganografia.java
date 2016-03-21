27a3807b36cc3513c7ae2ad9ee6cefce8a81f18b
Kaio <kaio.kassiano.oliveira@ccc.ufcg.edu.br>
2016-03-20 23:20:17 -0300

    Exceptions working properly

--- bin/Esteganografia.java
+++ bin/Esteganografia.java
@@ -1,34 +1,38 @@
 package esteganografia_mat_discreta;
 
-import org.farng.mp3.MP3File;
-import org.farng.mp3.id3.AbstractID3v2;
+import java.io.IOException;
 
-// #LiberaOPushKaio
+import org.farng.mp3.TagException;
+
 public class Esteganografia {
 
 	private static Vigenere vigenere;
+	private static ID3 id3;
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, TagException {
 
+		id3 = new ID3();
 		vigenere = new Vigenere();
 
 		int opcao = vigenere.leituraOpcao("O que deseja fazer?\n1- Encriptar\n2- Decriptar\nOpcao: ");
 
 		switch (opcao) {
-		case 1:
-			String mensagemOriginal = vigenere.leituraMensagem("\nDigite a mensagem a ser ocultada: ");
-			String chaveEncriptar = vigenere.leituraMensagem("Digite a chave: ");
 
-			String encriptar = Vigenere.encrypt(mensagemOriginal, chaveEncriptar);
-			System.out.println(encriptar);
+		case 1:
+
+			try {
+				String mensagemOriginal = vigenere.leituraMensagem("\nDigite a mensagem a ser ocultada: ");
+				String encriptar = Vigenere.encrypt(mensagemOriginal);
+				id3.adicionaMensagemAoArquivo(encriptar);
+			} catch (Exception e) {
+				System.out.println(e);
+			}
 			break;
 
 		case 2:
-			String mensagemEncriptada = vigenere.leituraMensagem("\nDigite a mensagem a ser recuperada: ");
-			String chaveDecriptar = vigenere.leituraMensagem("Digite a chave: ");
 
-			String decriptar = Vigenere.decrypt(mensagemEncriptada, chaveDecriptar);
-			System.out.println(decriptar);
+			String decriptar = id3.getFrameMensagem();
+			System.out.println("\n" + Vigenere.decrypt(decriptar));
 			break;
 		}
 	}
